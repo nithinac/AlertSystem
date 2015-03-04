48b386a5c7097b89679f52a22e43e156999bd750
nithinac <nithinnayagamac@gmail.com>
2015-03-04 15:49:49 +0530

    commit 7

---
+++ AlertSystemMVC/AlertSystemMVC/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.Owin;
+using Owin;
+
+namespace AlertSystemMVC
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHangfire(config =>
+            {
+                config.UseSqlServerStorage("AlertSystemMVCContext");
+                config.UseServer();
+            });
+        }
+    }
+}
